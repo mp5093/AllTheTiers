b7f0e58dc888a8c1ec3630caf62b01f1076c1a80
lukejdav <lukejdav@gmail.com>
2023-09-05 11:51:33 +0100

    Internal wind turbine and multipurpose assembler

--- components/power.lua
+++ components/power.lua
@@ -1,2 +1,16 @@
 table.insert(data["techs"]["t_ldframe"].unlocks, "c_large_power_relay")
 table.insert(data["techs"]["t_humanproduction"].unlocks, "c_large_power_transmitter")
+table.insert(data["techs"]["t_power10"].unlocks, "c_wind_turbine_i")
+
+data.components.c_wind_turbine_i = data.components.c_wind_turbine:RegisterComponent("c_wind_turbine_i", {
+	name = "Internal Wind Turbine",
+	texture = "AllTheTiers/textures/icons/components/Component_WindTurbine_01_I.png",
+	desc = "A small internal wind turbine, <hl>25</> normally, <hl>50</> on the plateau",
+	race = "robot",
+    production_recipe = CreateProductionRecipe({ circuit_board = 2, energized_plate = 2, wire = 2 }, {c_assembler = 40 }),
+    activation = "Always",
+	adjust_extra_power = true,
+	attachment_size = "Internal",
+	production_sound = "fx_assembler",
+	max_power = 5,
+})
--- components/production.lua
+++ components/production.lua
@@ -1,4 +1,4 @@
-table.insert(data["techs"]["t_robotics2"].unlocks, "c_assembler_big")
+table.insert(data["techs"]["t_storage2"].unlocks, "c_assembler_big")
 
 data.components.c_assembler_big = data.components.c_fabricator:RegisterComponent("c_assembler_big", {
 	name = "Multipurpose Assembler",
@@ -9,34 +9,123 @@ data.components.c_assembler_big = data.components.c_fabricator:RegisterComponent
 	attachment_size = "Large",
 	production_sound = "fx_assembler",
 	power = -30,
-	production_recipe = CreateProductionRecipe({ reinforced_plate = 20, cable = 10, icchip = 5 }, {c_assembler = 100 }),
+	production_recipe = CreateProductionRecipe({ energized_plate = 20, optic_cable = 20, icchip = 10 }, {c_assembler = 100 }),
 })
 data.visuals.v_assembler_01_m1  = { mesh = "StaticMesh'/Game/Meshes/BaseBuildings/Component_Assembler_01_M.Component_Assembler_01_M'", 
-    light_color = { 0.8, 0.8, 0.8, 0.8 }
+	light_color = { 0.8, 0.8, 0.8, 0.8 }
 }
 
 for k, v in pairs( data.items ) do
 	if data.items[ k ].production_recipe == nil then
 	else
-        if data.items[ k ].production_recipe.producers.c_robotics_factory == nil then
+
+		if data.items[ k ].production_recipe.producers.c_data_analyzer == nil then
 		else
 			data.items[ k ].production_recipe.producers.c_assembler_big = 
-                        data.items[ k ].production_recipe.producers.c_robotics_factory/2
-        end
+						data.items[ k ].production_recipe.producers.c_data_analyzer
+		end
+
+		if data.items[ k ].production_recipe.producers.c_robotics_factory == nil then
+		else
+			data.items[ k ].production_recipe.producers.c_assembler_big = 
+						data.items[ k ].production_recipe.producers.c_robotics_factory/2
+		end
+
 		if data.items[ k ].production_recipe.producers.c_refinery == nil then
 		else
 			data.items[ k ].production_recipe.producers.c_assembler_big = 
-                        data.items[ k ].production_recipe.producers.c_refinery/2
-        end
-        if data.items[ k ].production_recipe.producers.c_assembler == nil then
+						data.items[ k ].production_recipe.producers.c_refinery/2
+		end
+
+		if data.items[ k ].production_recipe.producers.c_assembler == nil then
 		else
 			data.items[ k ].production_recipe.producers.c_assembler_big = 
-                        data.items[ k ].production_recipe.producers.c_assembler/2
-        end
-        if data.items[ k ].production_recipe.producers.c_fabricator == nil then
+						data.items[ k ].production_recipe.producers.c_assembler/2
+		end
+
+		if data.items[ k ].production_recipe.producers.c_fabricator == nil then
 		else
 			data.items[ k ].production_recipe.producers.c_assembler_big = 
-                        data.items[ k ].production_recipe.producers.c_fabricator/4
-        end
-    end
+						data.items[ k ].production_recipe.producers.c_fabricator/4
+		end
+
+	end
+end
+
+for k, v in pairs( data.components ) do
+	if data.components[ k ].production_recipe == nil then
+	else
+		if data.components[ k ].production_recipe == false then
+		else
+
+			if data.components[ k ].production_recipe.producers.c_data_analyzer == nil then
+			else
+				data.components[ k ].production_recipe.producers.c_assembler_big = 
+							data.components[ k ].production_recipe.producers.c_data_analyzer
+			end
+
+			if data.components[ k ].production_recipe.producers.c_robotics_factory == nil then
+			else
+				data.components[ k ].production_recipe.producers.c_assembler_big = 
+							data.components[ k ].production_recipe.producers.c_robotics_factory/2
+			end
+
+			if data.components[ k ].production_recipe.producers.c_refinery == nil then
+			else
+				data.components[ k ].production_recipe.producers.c_assembler_big = 
+							data.components[ k ].production_recipe.producers.c_refinery/2
+			end
+
+			if data.components[ k ].production_recipe.producers.c_assembler == nil then
+			else
+				data.components[ k ].production_recipe.producers.c_assembler_big = 
+							data.components[ k ].production_recipe.producers.c_assembler/2
+			end
+
+			if data.components[ k ].production_recipe.producers.c_fabricator == nil then
+			else
+				data.components[ k ].production_recipe.producers.c_assembler_big = 
+							data.components[ k ].production_recipe.producers.c_fabricator/4
+			end
+		end
+	end
+end
+
+for k, v in pairs( data.frames ) do
+	if data.frames[ k ].production_recipe == nil then
+	else
+		if data.frames[ k ].production_recipe == false then
+		else
+
+			if data.frames[ k ].production_recipe.producers.c_data_analyzer == nil then
+			else
+				data.frames[ k ].production_recipe.producers.c_assembler_big = 
+							data.frames[ k ].production_recipe.producers.c_data_analyzer
+			end
+
+			if data.frames[ k ].production_recipe.producers.c_robotics_factory == nil then
+			else
+				data.frames[ k ].production_recipe.producers.c_assembler_big = 
+							data.frames[ k ].production_recipe.producers.c_robotics_factory/2
+			end
+
+			if data.frames[ k ].production_recipe.producers.c_refinery == nil then
+			else
+				data.frames[ k ].production_recipe.producers.c_assembler_big = 
+							data.frames[ k ].production_recipe.producers.c_refinery/2
+			end
+
+			if data.frames[ k ].production_recipe.producers.c_assembler == nil then
+			else
+				data.frames[ k ].production_recipe.producers.c_assembler_big = 
+							data.frames[ k ].production_recipe.producers.c_assembler/2
+			end
+
+			if data.frames[ k ].production_recipe.producers.c_fabricator == nil then
+			else
+				data.frames[ k ].production_recipe.producers.c_assembler_big = 
+							data.frames[ k ].production_recipe.producers.c_fabricator/4
+			end
+		end
+	end
 end
