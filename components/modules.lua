64bffe6774de226e6e52d4cee0ca356327ff470d
lukejdav <lukejdav@gmail.com>
2023-09-05 09:23:46 +0100

    Unlock bigger modules

---
+++ components/modules.lua
@@ -0,0 +1,12 @@
+
+table.insert(data["techs"]["t_robotics4"].unlocks, "c_modulehealth_m")
+table.insert(data["techs"]["t_robotics4"].unlocks, "c_modulehealth_l")
+
+table.insert(data["techs"]["t_robotics4"].unlocks, "c_modulevisibility_m")
+table.insert(data["techs"]["t_robotics4"].unlocks, "c_modulevisibility_l")
+
+table.insert(data["techs"]["t_robotics4"].unlocks, "c_moduleefficiency_m")
+table.insert(data["techs"]["t_robotics4"].unlocks, "c_moduleefficiency_l")
+
+table.insert(data["techs"]["t_robotics4"].unlocks, "c_modulespeed_m")
+table.insert(data["techs"]["t_robotics4"].unlocks, "c_modulespeed_l")
